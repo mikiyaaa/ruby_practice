6d50514a524358c331e045e01e7d8bd29433e60a
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-05-23 23:51:52 +0900

    each_with_indexによる繰り返しと検索

---
+++ drill7.rb
@@ -0,0 +1,14 @@
+# each_with_indexメソッド
+
+def search(target_num, input)
+  input.each_with_index do |num, index|
+    if num == target_num
+      puts "#{index + 1}番目にあります"
+      return
+    end
+    puts "その数は含まれていません"
+  end
+end
+
+input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
+search(3, input)
