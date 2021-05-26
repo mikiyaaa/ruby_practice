cdb4645bc41c988c20c84517041060af90a850e8
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-05-26 23:37:29 +0900

    ビンゴカードアプリのリファクタリング

--- bingo.rb
+++ bingo.rb
@@ -1,25 +1,13 @@
-def word
-  [" B", " I", " N", " G", " O"].join(" | ")
+
+
+def card
+  bingo = ["BINGO".chars]
+  numbers = (1..75).each_slice(15).map { |number| number.sample(5) }.transpose
+  numbers[2][2] = "☆ "
+  rows = bingo + numbers
+  rows.map { |row|
+    row.map { |r| r.to_s.rjust(2) }.join(" | ")  
+}.join("\n")
 end
 
-
-B = (1..15).to_a
-I = (16..30).to_a
-N = (31..45).to_a
-G = (46..60).to_a
-O = (61..75).to_a
-
-bingos = (1..75).each_slice(15).map { |bingo| 
-  bingo.sample(5) }.transpose
-}
-
-def number
-  bingos = [B, I, N, G, O].map { |bingo| bingo.sample(5)}.transpose
-  bingos[2][2] = "☆ "
-  bingos.map { |bin|
-    bin.map { |b| b.to_s.rjust(2) }.join(" | ")
-  }.join("\n")
-end
-
-puts word
-puts number
+puts card
